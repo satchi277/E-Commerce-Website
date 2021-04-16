1461184dbd49423acbe067c5137f546a40ab252a
Satchi277 <sahasatchidananda277@gmail.com>
2021-04-16 12:22:52 +0530

    Create Function :-

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -10,4 +10,8 @@ class AdminController extends Controller
     public function dashboard(){
         return view('admin.admin_dashboard');
     }
+
+    public function login(){
+        return view('admin.admin_login');
+    }
 }
