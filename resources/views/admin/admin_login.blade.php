86c687aff2f7c7f5a081011263f085d4e1fc7139
Satchi277 <sahasatchidananda277@gmail.com>
2021-04-18 02:34:35 +0530

    Update admin_login file

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -23,7 +23,7 @@
     </div>
     <div class="card-body">
   @if(Session::has('error_message'))
-      <div class="alert alert-danger fade show" role="alert">
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
         {{ Session::get('error_message') }}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
