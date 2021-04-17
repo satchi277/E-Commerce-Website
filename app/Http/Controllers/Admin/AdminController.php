f2efb79b6d99e13c6344c9a31ece2740c1df9d08
Satchi277 <sahasatchidananda277@gmail.com>
2021-04-18 02:29:31 +0530

    Update login function at AdminController file :- ogin function at AdminController.add the condition for posted data and use guard for admin login.create logout function in AdminController.

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Session;
+use Auth;
 
 class AdminController extends Controller
 {
@@ -11,7 +13,23 @@ class AdminController extends Controller
         return view('admin.admin_dashboard');
     }
 
-    public function login(){
+    public function login(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->all();
+            /* echo "<pre>"; print_r($data); die; */
+            if(Auth::guard('admin')->attempt(['email'=>$data['email'],'password'=>$data['password']])){
+                return redirect('admin/dashboard');
+            }
+            else{
+                //For Error Message
+                Session::flash('error_message','Invalid Email or Password');
+                return redirect()->back();
+            }
+        }
         return view('admin.admin_login');
+    }
+    public function logout(){
+        Auth::guard('admin')->logout();
+        return redirect('/admin');
     }
 }
